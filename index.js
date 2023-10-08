08c1ff1e2505dbaf6f2cef87ecc6b990d38b17ab
bony_musau <boniface.musau@moringaschool.com>
2023-10-08 14:18:22 +0300

    scope lab assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName='bob';
+
+function upperCaseCustomerCase(){
+    return customerName.toUpperCase();
+}
+console.log(upperCaseCustomerCase());
+
+var bestCustomer="not bob";
+function setBestCustomer(){
+    return bestCustomer;
+}
+console.log(setBestCustomer());
+
+function overwriteBestCustomer(){
+    if(overwriteBestCustomer != bestCustomer){
+        return 'maybe bob';
+   }
+}
+console.log(overwriteBestCustomer());
+
+const leastFavouriteCustomer="john";
+
+function changeLeastFavouriteCustomer(){
+    leastFavouriteCustomer=changeLeastFavouriteCustomer;
+}
+changeLeastFavouriteCustomer("jane")
